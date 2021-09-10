1e5394172b2755da8bf266dca5ba067c5ed72680
Joe Haines <josephthaines@gmail.com>
2021-09-10 11:35:06 -0600

    basic server implementation and front end example

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,61 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      productId: 47425,
+      displayProduct: [],
+    };
+    this.formatBody = this.formatBody.bind(this);
+
   }
 
+  formatBody(method, apiRoute, params, data) {
+
+    let bodyObj = {
+      method: method,
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${apiRoute}`,
+      data: data,
+      params: params,
+      headers: {Authorization: ''}
+    };
+
+
+
+    console.log('body', bodyObj);
+    return bodyObj;
+  }
+
+  componentDidMount() {
+
+    // eslint-disable-next-line quotes
+    let body = this.formatBody('GET', `/products/${this.state.productId}`);
+
+    axios.post('/api/*', body).then((results)=>{
+      console.log('results', results);
+      this.setState({displayProduct: results.data});
+      console.log('this.state', this.state);
+
+    });
+
+  }
   render() {
     return (
       <React.Fragment>
-        <h1>Welcome to FEC Project Atelier</h1>
+        <h1>Welcome to FEC Project Atelier </h1>
+        <h2> Name: {this.state.displayProduct.name}</h2>
+        <h2> <p> Description: {this.state.displayProduct.description}</p></h2>
+        <h2> <p> Price: ${this.state.displayProduct.default_price}</p></h2>
       </React.Fragment>
     );
   }
 }
 
 export default App;
+
+
+
+
--- server.js
+++ server.js
@@ -1,11 +1,39 @@
-const path = require("path")
-const express = require("express");
+const path = require('path');
+const express = require('express');
+const config = require('./config');
+const token = config.token;
+const apiUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp';
+const axios = require('axios');
+const _ = require('underscore');
 
 const app = express();
 const port = 3000;
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 // other configuration...
+
+
+app.post('/api/*', (req, res)=>{
+  console.log('body', req.body);
+
+  let options = req.body;
+  options.headers.Authorization = token;
+
+
+  axios(options).then((results)=>{
+    console.log('API Results:', results.data);
+    res.send(results.data);
+  }).catch((err)=>{
+    console.log('API Error:', err);
+    res.send(err);
+  });
+
+
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Express Server is running on port ${port}`);
