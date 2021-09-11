006760ebd17daa94195c5b56ba77ed2956e04330
Ashequl Haque <ashequlhaque009@gmail.com>
2021-09-10 23:23:38 -0400

    changed folder name from Q&A to QnA

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import '../fa-icons/fa-icons.js';
-import QuestionsNAnswersContainer from './Q&A/Questions&AnswersContainer.jsx';
+import QuestionsNAnswersContainer from './QnA/Questions&AnswersContainer.jsx';
 
 
 class App extends React.Component {
