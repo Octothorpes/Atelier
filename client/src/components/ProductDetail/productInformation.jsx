6a93615bced6bd966f81dc5d5c3b7cf5d1a052b7
Joe Haines <josephthaines@gmail.com>
2021-09-13 00:28:31 -0600

    got quantity selector to get relevent data from size selector

--- client/src/components/ProductDetail/productInformation.jsx
+++ client/src/components/ProductDetail/productInformation.jsx
@@ -2,30 +2,60 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import dummydata from './productInformationDummy.js';
 
+import _ from 'underscore';
+
 const productStyles = dummydata.productStyles;
 const productInfo = dummydata.productInfo;
 
 class ProductInformation extends React.Component {
   constructor(props) {
     super(props);
-
+    // var styleSkus = productStyles.filter(id => id === this.state.checkedId);
     this.state = {
+      productId: productInfo.id,
       productStyles: productStyles,
       productInfo: [productInfo],
       defaultStyle: productStyles[0].name,
       originalPrice: productStyles[0].original_price,
       checkedId: productStyles[0].style_id,
+      Skus: [productStyles[0].skus],
+      SkusObj: productStyles[0].skus,
+      quantity: 0,
     };
 
     this.styleClickHandler = this.styleClickHandler.bind(this);
+    this.sizeAndQuantityClickHandler =
+      this.sizeAndQuantityClickHandler.bind(this);
   }
 
   styleClickHandler(e, originalPrice, salesprice, def) {
     console.log(e.target['id']);
     const newCheckedId = Number(e.target['id']);
     const activeClass = this.state.active;
-    this.setState({ defaultStyle: e.target.name, originalPrice, checkedId: newCheckedId });
+    this.setState({
+      defaultStyle: e.target.name,
+      originalPrice,
+      checkedId: newCheckedId,
+    });
   }
+
+  sizeAndQuantityClickHandler(e) {
+    let idx = e.target.selectedIndex;
+    let skuId = Number(e.target.options[idx]['id']);
+    let size = e.target.options[idx].value;
+    let quantity = Number(e.target.options[idx].dataset.quantity);
+
+    console.log('Style and click', idx, skuId, size, quantity);
+    this.setState({ quantity: quantity });
+  }
+
+  // componentDidMount() {
+  //   let selectedSkus = productStyles.filter(function (styleId) {
+  //     return styleId.style_id === productStyles[0].style_id;
+  //   });
+  //   this.setState({Skus:selectedSkus})
+  //   console.log('skus', selectedSkus);
+  // }
 
   render() {
     return (
@@ -49,6 +79,14 @@ class ProductInformation extends React.Component {
           productStyles={this.state.productStyles}
           photos={this.state.productStyles[1].photos}
           styleClickHandler={this.styleClickHandler}
+        />
+        <SizeAndQuantitySelector
+          quantity={this.state.quantity}
+          sizeAndQuantityClickHandler={this.sizeAndQuantityClickHandler}
+          productStyles={this.state.productStyles}
+          SkusObj={this.state.SkusObj}
+          selectedSkus={this.state.Skus}
+          checkedId={this.state.checkedId}
         />
       </div>
     );
@@ -77,7 +115,6 @@ let CategoryName = function (props) {
 
 let StyleSelector = function (props) {
   let photos = props.photos;
-
 
   let mappedStyles = props.productStyles.map((style) => {
     return (
@@ -115,4 +152,62 @@ let StyleSelector = function (props) {
     </div>
   );
 };
+
+let SizeAndQuantitySelector = function (props) {
+  let options = props.selectedSkus.map((sku) => {
+    return Object.values(sku);
+  });
+  options = options.flat(1);
+
+  let test = _.pairs(props.SkusObj);
+  let test2 = options.map((object) => {
+    return _.range(1, object.quantity + 1).filter((quantity) => quantity <= 15);
+  });
+
+  let quantityRange = _.range(1, props.quantity + 1).filter(
+    (quantity) => quantity <= 15
+  );
+
+  let test3 = props.productStyles
+    .filter((styleObj) => {
+      return styleObj.style_id === props.checkedId;
+    })
+    .map((styleObj) => _.pairs(styleObj.skus));
+
+  // console.log('test3', test3);
+  console.log('test', quantityRange);
+  // console.log('test2', test2);
+
+  // console.log('here', opt, 'skus',props.selectedSkus[0][0]);
+  return (
+    <div className='size-quantity-container'>
+      <select
+        className='size-selector'
+        onChange={(e) => props.sizeAndQuantityClickHandler(e)}>
+        <option> Select Size</option>
+        {test.map((sku) => {
+          return (
+            <option
+              key={sku[0]}
+              id={sku[0]}
+              data-quantity={sku[1].quantity}
+              value={sku[1].size}>
+              {' '}
+              {sku[1].size}
+            </option>
+          );
+        })}
+      </select>
+
+      <select placeholder="whatever">
+        {quantityRange.length === 0 ? (
+          <option>{'---'}</option>
+        ) : (
+          quantityRange.map((num) => <option key={num}>{num}</option>)
+        )}
+      </select>
+    </div>
+  );
+};
+
 export default ProductInformation;
--- client/src/components/ProductDetail/productInformationDummy.js
+++ client/src/components/ProductDetail/productInformationDummy.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 /* eslint-disable camelcase */
 module.exports.productStyles = [
   {
