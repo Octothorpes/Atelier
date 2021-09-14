c4f5d597585d7290fad918ea55f91a2b50665d7b
Joe Haines <josephthaines@gmail.com>
2021-09-13 22:48:15 -0600

    test

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,9 +2,12 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import RnR from './RnR/RnR.jsx';
 import '../fa-icons/fa-icons.js';
+import RelatedProducts from './RelatedProducts/RelatedProducts.jsx';
+import OutfitProducts from './RelatedProducts/OutfitProducts.jsx';
 import QuestionsNAnswersContainer from './QnA/Questions&AnswersContainer.jsx';
-import ProductDetailContainer from './ProductDetail/productDetailContainer.jsx';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -25,36 +28,44 @@ class App extends React.Component {
       headers: { Authorization: '' },
     };
 
-    // console.log('body', bodyObj);
+    console.log('body', bodyObj);
     return bodyObj;
   }
 
   componentDidMount() {
     // eslint-disable-next-line quotes
     let body = this.formatBody('GET', `/products/${this.state.productId}`);
-    // axios
-    //   .post('/api/*', body)
-    //   .then((results) => {
-    //     // console.log('results', results);
-    //     this.setState({ displayProduct: results.data });
-    //     // console.log('this.state', this.state);
-    //   })
-    //   .catch((err) => {
-    //     console.log('error', err);
-    //   });
+    axios
+      .post('/api/*', body)
+      .then((results) => {
+        console.log('results', results);
+        this.setState({ displayProduct: results.data });
+        console.log('this.state', this.state);
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+
   }
+
   render() {
+
     return (
       <React.Fragment>
-        <ProductDetailContainer formatBody={this.formatBody} productId ={this.state.productId}/>
-        {/* <h1>Welcome to FEC Project Atelier</h1>
+
+        <h1>Welcome to FEC Project Atelier</h1>
         <h2> Name: {this.state.displayProduct.name}</h2>
         <h2>
           <p> Description: {this.state.displayProduct.description}</p>
         </h2>
         <h2>
           <p> Price: ${this.state.displayProduct.default_price}</p>
-        </h2> */}
+        </h2>
+        <h3 className="related-prod">Related products: <RelatedProducts relatedProd={this.state.displayProduct}/></h3>
+        <h3 className="related-prod">OutfitProducts:</h3><OutfitProducts />
+
+
+        <RnR/>
         <QuestionsNAnswersContainer />
       </React.Fragment>
     );
