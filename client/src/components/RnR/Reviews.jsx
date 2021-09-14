1572205da7b6e973d6f2ff114e3cd239dace42fb
Aaron Fife <aafife55@gmail.com>
2021-09-14 11:16:14 -0700

    invidual review tiles updated with most API data - still need minor css and jsx adjustments

--- client/src/components/RnR/Reviews.jsx
+++ client/src/components/RnR/Reviews.jsx
@@ -16,24 +16,34 @@ class Reviews extends React.Component {
     console.log('----->', reviews);
 
     let date, summary, recommend, response;
+    const starTile = {
+      '0': '☆☆☆☆☆',
+      '1': '★☆☆☆☆',
+      '2': '★★☆☆☆',
+      '3': '★★★☆☆',
+      '4': '★★★★☆',
+      '5': '★★★★★',
+    };
+
     if (reviews.results) {
       summary = reviews.results.map((item) => {
         date = new Date(item.date.slice(0, 10));
-        date = date.toString().slice(4, 15).insert();
+        date = date.toString().slice(4, 15);
+
         if (item.recommend) { recommend = '✔ I recommend this product'; }
         if (item.response) { response = item.response; }
 
         return (
           <div className="individualReviewBox" key={item.summary}>
             <div id="starsUsernameDate">
-              <p>☆☆☆☆☆</p>
+              <p>{starTile[item.rating.toString()]}</p>
               <p id="usernameDate">{item.reviewer_name}, {date}</p>
             </div>
             <div id="summaryHelpfulAndReport">
               <p id="summaryTitle">{item.summary}</p>
               <p id="summaryBody">{item.body}</p>
-              <p id="trueRecommend">{recommend || ''}</p>
-              <p id="trueResponse">{'Response:', response || ''}</p>
+              <p id="trueRecommend">{recommend}</p>
+              <p id="trueResponse">{'Response:', response}</p>
               <div id="helpfulAndReport">
                 <span>Helpful?</span>
                 <span><span className="yesAndReport">Yes</span> ({item.helpfulness})</span>
