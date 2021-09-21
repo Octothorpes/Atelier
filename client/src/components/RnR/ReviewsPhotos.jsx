c7c391260c7948bfdcf07cd394a21dedff138362
Aaron Fife <aafife55@gmail.com>
2021-09-20 20:26:15 -0700

    attempted creating a modal - didnt work, deleted all changes

--- client/src/components/RnR/ReviewsPhotos.jsx
+++ client/src/components/RnR/ReviewsPhotos.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Reviews.css';
-import Modal from './ReviewsTest.jsx';
 
 class ReviewsPhotos extends React.Component {
   constructor(props) {
@@ -8,16 +7,10 @@ class ReviewsPhotos extends React.Component {
     this.state = {
       photos: this.props.photos,
       photoZoom: false,
-      show: false
     };
-    this.showModal = this.showModal.bind(this);
-    this.hideModal = this.hideModal.bind(this);
+
     this.photoClickHandler = this.photoClickHandler.bind(this);
   }
-
-  showModal() { this.setState({ show: true }); }
-
-  hideModal() { this.setState({ show: false }); }
 
   photoClickHandler() {
     console.log('photo click test');
@@ -27,32 +20,24 @@ class ReviewsPhotos extends React.Component {
   render() {
     let photos;
 
-    if (!this.state.show) {
+    if (!this.state.photoZoom) {
       photos = this.state.photos.map((item, index) => {
         // if (index !== 0) { return; }
         return (
-          <img id="smallImage" src={this.state.photos[0].url} onClick={this.showModal} key={item.id}/>
+          <img id="smallImage" src={item.url} onClick={this.photoClickHandler} key={item.id}/>
         );
       });
+    } else {
+      photos = this.state.photos.map((item) => (
+        <img id="modalImage" src={item.url} key={item.id} onClick={this.photoClickHandler}/>
+      ));
     }
-    // else {
-    //   photos = this.state.photos.map((item) => (
-    //     <img id="modalImage" src={this.state.photos[0].url} key={item.id} onClick={this.photoClickHandler}/>
-    //   ));
-    // }
 
 
 
     return (
       <React.Fragment>
-        <div>
-          <main>
-            <Modal show={this.state.show} handleClose={this.hideModal}>
-              <p>test</p>
-              {photos}
-            </Modal>
-          </main>
-        </div>
+        {photos}
       </React.Fragment>
     );
   }
