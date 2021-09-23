aa65f549771f4aa919885d3060b1016ed4d3aa48
Aaron Fife <aafife55@gmail.com>
2021-09-23 11:22:31 -0700

    changed AddReviewModal to functional component

--- client/src/components/RnR/modals/AddReviewModal.jsx
+++ client/src/components/RnR/modals/AddReviewModal.jsx
@@ -5,84 +5,96 @@ import ModalCharacs from './components/ModalCharacs.jsx';
 import ModalReviewBody from './components/ModalReviewBody.jsx';
 import ModalUpload from './components/ModalUpload.jsx';
 
-const Modal = function(props) {
-  if (!props.show) {
-    return null;
+
+
+class AddReviewModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ''
+    };
+
   }
 
 
-  return (
-    <div className="image-modal">
-      <div className="image-modal-content">
-        <div className="image-modal-header">
-          <h4>Write Your Review</h4>
-          <h5>About the {props.productName || 'product'}</h5>
-        </div>
+  render () {
+    if (!this.props.show) { return null; }
 
-        <div className="image-modal-body">
-          <form>
-            <label>Overall Rating* </label>
-            <br></br>
-            <ModalStars/>
-            <br></br>
-            <br></br>
+    return (
+      <div className="image-modal">
+        <div className="image-modal-content">
+          <div className="image-modal-header">
+            <h4>Write Your Review</h4>
+            <h5>About the {this.props.productName || 'product'}</h5>
+          </div>
 
-            <label>Do you recommend this product?*</label>
-            <br></br>
-            <input type="radio" name="yesNo" defaultChecked/>
-            <label>yes</label>
-            <input type="radio" name="yesNo"/>
-            <label>no</label>
-            <br></br>
-            <br></br>
+          <div className="image-modal-body">
+            <form>
+              <label>Overall Rating* </label>
+              <br></br>
+              <ModalStars/>
+              <br></br>
+              <br></br>
 
-            <label>Characteristics*</label>
-            <br></br>
-            <ModalCharacs reviewsMeta={props.reviewsMeta}/>
-            <br></br>
-            <br></br>
+              <label>Do you recommend this product?*</label>
+              <br></br>
+              <input type="radio" name="yesNo" defaultChecked/>
+              <label>yes</label>
+              <input type="radio" name="yesNo"/>
+              <label>no</label>
+              <br></br>
+              <br></br>
 
-            <label>Review Summary*</label>
-            <br></br>
-            <textarea cols="60" rows="1" maxLength="60" placeholder="Example: Best purchase ever!"></textarea>
-            <br></br>
-            <br></br>
+              <label>Characteristics*</label>
+              <br></br>
+              <ModalCharacs reviewsMeta={this.props.reviewsMeta}/>
+              <br></br>
 
-            <label>Review Body*</label>
-            <br></br>
-            <ModalReviewBody/>
-            <br></br>
-            <br></br>
+              <label>Review Summary*</label>
+              <br></br>
+              <textarea cols="60" rows="1" maxLength="60" placeholder="Example: Best purchase ever!"></textarea>
+              <br></br>
+              <br></br>
 
-            <label>Upload Photos</label>
-            <br></br>
-            <ModalUpload photos={[]}/>
-            <br></br>
-            <br></br>
+              <label>Review Body*</label>
+              <br></br>
+              <ModalReviewBody/>
+              <br></br>
+              <br></br>
 
-            <label>Your nickname*</label>
-            <br></br>
-            <input type="text" maxLength="60" placeholder="Example: jackson11!"/>
-            <br></br>
-            <i>For privacy reasons, do not use your full name or email address</i>
-            <br></br>
-            <br></br>
+              <label>Upload Photos</label>
+              <br></br>
+              <ModalUpload photos={[]}/>
+              <br></br>
+              <br></br>
 
-            <label>Your email*</label>
-            <br></br>
-            <input type="text" maxLength="60" placeholder="Example: jackson11@email.com"/>
-            <br></br>
-            <i>For authentication reasons, you will not be emailed</i>
-          </form>
-        </div>
+              <label>Your nickname*</label>
+              <br></br>
+              {/* <input type="text" maxLength="60" placeholder="Example: jackson11!"/> */}
+              <textarea cols="40" rows="1" maxLength="60" placeholder="Example: jackson11!"></textarea>
 
-        <div className="image-modal-footer">
-          <button className="image-button" onClick={props.show}>cancel</button>
-          <button className="image-button" onClick={props.show}>submit</button>
+              <br></br>
+              <i>For privacy reasons, do not use your full name or email address</i>
+              <br></br>
+              <br></br>
+
+              <label>Your email*</label>
+              <br></br>
+              {/* <input type="text" maxLength="60" placeholder="Example: jackson11@email.com"/> */}
+              <textarea cols="40" rows="1" maxLength="60" placeholder="Example: jackson11@email.com"></textarea>
+              <br></br>
+              <i>For authentication reasons, you will not be emailed</i>
+            </form>
+          </div>
+
+          <div className="image-modal-footer">
+            <button className="image-button" onClick={this.props.show}>cancel</button>
+            <button className="image-button" onClick={this.props.show}>submit</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default Modal;
+export default AddReviewModal;
