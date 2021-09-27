6b9d4275981c8d198a2e8d30d0c9da88e779adca
Joe Haines <josephthaines@gmail.com>
2021-09-27 16:41:16 -0600

    added some unit tests for most components, image gallery and gallery modal still needed

--- client/src/components/ProductDetail/ProductDetail.test.js
+++ client/src/components/ProductDetail/ProductDetail.test.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+library.add(faCoffee);
+library.add(faSearch);
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import renderer from 'react-test-renderer';
 import SearchBar from './searchBar.jsx';
 import Features from './features.jsx';
 import ProductDescriptionAndFeatures from './productDescriptionAndFeatures.jsx';
-import { ShallowWrapper } from 'enzyme/build';
+import productDetailContainer from './productDetailContainer.jsx';
+// import { ShallowWrapper } from 'enzyme/build';
 import CheckboxWithLabel from './checkboxWithLabel';
-import axios from 'axios';
-jest.mock('axios');
+import toJson from 'enzyme-to-json';
+// import axios from 'axios';
+// jest.mock('axios');
 
 describe('<SearchBar />', () => {
   test('it should render correctly with no props', () => {
     const wrapper = shallow(<SearchBar />);
 
-    expect(wrapper).toMatchSnapshot();
+    expect(
+      toJson(wrapper, {
+        noKey: false,
+        ignoreDefaultProps: true,
+        mode: 'shallow',
+      })
+    ).toMatchSnapshot();
   });
 
   test('it should have two children with the className .mockSearchIcon', () => {
     const wrapper = shallow(<SearchBar />);
+
 
     expect(wrapper.find('.mockSearchIcon')).toHaveLength(2);
   });
--- client/src/components/ProductDetail/sizeAndQuantitySelector.test.js
+++ client/src/components/ProductDetail/sizeAndQuantitySelector.test.js
@@ -23,16 +23,121 @@ import _ from 'underscore';
 //   selectedSize: 'Select Size',
 //   selectedQuantity: 1,
 // };
+
+let skus = {
+  1665179: {
+    quantity: 0,
+    size: '7',
+  },
+  1665180: {
+    quantity: 0,
+    size: '7.5',
+  },
+  1665181: {
+    quantity: 0,
+    size: '8',
+  },
+  1665182: {
+    quantity: 0,
+    size: '8.5',
+  },
+  1665183: {
+    quantity: 0,
+    size: '9',
+  },
+  1665184: {
+    quantity: 0,
+    size: '9.5',
+  },
+  1665185: {
+    quantity: 0,
+    size: '10',
+  },
+  1665186: {
+    quantity: 0,
+    size: '10.5',
+  },
+  1665187: {
+    quantity: 0,
+    size: '11',
+  },
+  1665188: {
+    quantity: 0,
+    size: '11.5',
+  },
+  1665189: {
+    quantity: 0,
+    size: '12',
+  },
+};
 const props = {
-
   selectedSize: 'Select Size',
-
+  hasStock: false,
+  sizeMenu: 1,
+  SkusObj: skus,
+  quantity: 0,
+  sendInteraction: () => {
+    return 'Success';
+  },
+  quantityOnChange: (e) => {
+    return e;
+  },
+  sizeAndQuantityClickHanlder: (e) => {
+    return e;
+  },
+  selectedQuantity: 1,
+};
+const props2 = {
+  selectedSize: 'Select Size',
+  hasStock: true,
+  sizeMenu: 1,
+  SkusObj: skus,
+  quantity: 0,
+  sendInteraction: () => {
+    return 'Success';
+  },
+  quantityOnChange: (e) => {
+    return e;
+  },
+  sizeAndQuantityClickHanlder: (e) => {
+    return e;
+  },
+  selectedQuantity: 1,
+};
+const props3 = {
+  selectedSize: '7',
+  hasStock: true,
+  sizeMenu: 1,
+  SkusObj: skus,
+  quantity: 14,
+  sendInteraction: () => {
+    return 'Success';
+  },
+  quantityOnChange: (e) => {
+    return e;
+  },
+  sizeAndQuantityClickHanlder: (e) => {
+    return e;
+  },
+  selectedQuantity: 1,
 };
 // npm test sizeAndQuantitySelector.test.js
 
 describe('<SizeAndQuantitySelector/>', () => {
-  test('basic', () => {
-    let wrapper = ShallowWrapper(<SizeAndQuantitySelector {...props} />);
-    expect(wrapper.find('.size-selector').options[0].value).toMatch(/Select Size/);
+  xtest('Style selector should display an Out of Stock message if there is no Stock', () => {
+    let wrapper = mount(<SizeAndQuantitySelector {...props} />);
+    expect(wrapper.find('.size-selector').render().text()).toMatch(/OUT OF STOCK/);
+  });
+  xtest('Style Selector should display Select Size if there is stock', () => {
+    let wrapper = mount(<SizeAndQuantitySelector {...props2} />);
+    expect(wrapper.find('.size-selector').render().text()).toMatch(/Select Size/);
+  });
+  xtest('Quantity selector should default to 1 if a size has been selected', () => {
+    let wrapper = mount(<SizeAndQuantitySelector {...props} />);
+    expect(wrapper.find('.quantity-selector').render().text()).toMatch(/---/);
+  });
+  test('Quantity selector should default to 1 if a size has been selected', () => {
+    let wrapper = mount(<SizeAndQuantitySelector {...props3} />);
+    expect(wrapper.find('.quantity-selector').render().text()).toMatch(/1/);
   });
 });
