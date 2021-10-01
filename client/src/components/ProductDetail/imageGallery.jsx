81d147cff2acba38e0455d4e416ca01ef1fd643f
Joe Haines <josephthaines@gmail.com>
2021-09-30 20:47:18 -0600

    styling for productInformation

--- client/src/components/ProductDetail/imageGallery.jsx
+++ client/src/components/ProductDetail/imageGallery.jsx
@@ -52,40 +52,39 @@ class Tracker extends React.Component {
     return (
       <React.Fragment>
         {/* <GalleryModal thumbnailClick ={this.props.thumbnailClick} image={this.props.image} selectedPhotos={this.props.selectedPhotos} selectedIndex={this.props.selectedThumbIndex}/> */}
-        <div className='icon-buttons-container'>
-          <button
-            onClick={(e) => {
-              this.props.sendInteraction('Image Gallery Default View');
-              this.props.arrowClick(e);
-            }}
-
-            className='icon-buttons'
-            id='left-arrow'
-            aria-label='Move to the previous image'
-            style={
-              this.props.selectedThumbIndex === 0
-                ? { visibility: 'hidden' }
-                : { visibility: 'visible' }
-            }>
-            <FontAwesomeIcon id='left-arrow-icon' icon='arrow-left' />{' '}
-          </button>
-          <button
-            onClick={(e) => {
-              this.props.sendInteraction('Image Gallery Default View');
-              this.props.arrowClick(e);
-            }}
-            className='icon-buttons'
-            id='right-arrow'
-            aria-label='Move to the next image'
-            style={
-              this.props.selectedThumbIndex ===
-              this.props.selectedPhotos.length - 1
-                ? { visibility: 'hidden' }
-                : { visibility: 'visible' }
-            }>
-            <FontAwesomeIcon id='right-arrow-icon' icon='arrow-right' />
-          </button>{' '}
-        </div>
+        {/* <div className='icon-buttons-container'> */}
+        <button
+          onClick={(e) => {
+            this.props.sendInteraction('Image Gallery Default View');
+            this.props.arrowClick(e);
+          }}
+          className='icon-buttons-left'
+          id='left-arrow'
+          aria-label='Move to the previous image'
+          style={
+            this.props.selectedThumbIndex === 0
+              ? { visibility: 'hidden' }
+              : { visibility: 'visible' }
+          }>
+          <FontAwesomeIcon id='left-arrow-icon' icon='arrow-left' />{' '}
+        </button>
+        <button
+          onClick={(e) => {
+            this.props.sendInteraction('Image Gallery Default View');
+            this.props.arrowClick(e);
+          }}
+          className='icon-buttons-right'
+          id='right-arrow'
+          aria-label='Move to the next image'
+          style={
+            this.props.selectedThumbIndex ===
+            this.props.selectedPhotos.length - 1
+              ? { visibility: 'hidden' }
+              : { visibility: 'visible' }
+          }>
+          <FontAwesomeIcon id='right-arrow-icon' icon='arrow-right' />
+        </button>{' '}
+        {/* </div> */}
         <div className={this.state.defaultClass}>
           <img
             onClick={() => {
@@ -93,24 +92,40 @@ class Tracker extends React.Component {
               this.props.displayModal();
             }}
             className={'default-view-image'}
-            alt={this.props.defaultStyle}
+            // alt={this.props.defaultStyle}
             src={this.props.image}></img>
         </div>
-
+        <button
+          onClick={(e) => {
+            this.props.sendInteraction('Image Gallery Default View');
+            this.props.arrowClick(e);
+          }}
+          name='Select the next Image Up'
+          id='arrow-up'
+          className='chevron-up'
+          aria-label='Button up '>
+          <FontAwesomeIcon
+            id='arrow-up-icon'
+            icon='chevron-up'
+            name='Select the next Image Up Icon'
+          />{' '}
+        </button>
+        <button
+          onClick={(e) => {
+            this.props.sendInteraction('Image Gallery Default View');
+            this.props.arrowClick(e);
+          }}
+          name='Select the next Image Down'
+          id='arrow-down'
+          className='chevron-down'
+          aria-label='Button up '>
+          <FontAwesomeIcon
+            id='arrow-down-icon'
+            icon='chevron-down'
+            name='Select the next Image Down Icon'
+          />
+        </button>
         <div className='image-thumbnails'>
-          <button
-            onClick={(e) => {
-              this.props.sendInteraction('Image Gallery Default View');
-              this.props.arrowClick(e);
-            }}
-            name='Select the next Image Up'
-            id='arrow-up'
-            className='chevron'
-            aria-label="Button up "
-           >
-
-            <FontAwesomeIcon id='arrow-up-icon' icon='chevron-up'  name='Select the next Image Up Icon' />{' '}
-          </button>
           {this.props.selectedPhotos.map((photo, i) => {
             return (
               <img
@@ -124,7 +139,7 @@ class Tracker extends React.Component {
                     ? {
                         borderBottom: '4px solid green',
                         backgroundColor: 'white',
-                        opacity: '.5',
+                        opacity: '1',
                         boxShadow: '0px 12px 22px 1px #333',
                       }
                     : { borderBottom: 'none' }
@@ -135,20 +150,6 @@ class Tracker extends React.Component {
                 src={photo.thumbnail_url}></img>
             );
           })}
-
-          <button
-            onClick={(e) => {
-              this.props.sendInteraction('Image Gallery Default View');
-              this.props.arrowClick(e);
-            }}
-            name='Select the next Image Down'
-            id='arrow-down'
-            className='chevron'
-            aria-label="Button up "
-            >
-
-            <FontAwesomeIcon id='arrow-down-icon' icon='chevron-down' name='Select the next Image Down Icon'  />
-          </button>
         </div>
       </React.Fragment>
     );
